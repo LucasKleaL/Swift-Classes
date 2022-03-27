7f2dfe71e6e66622649e6eba0a9dde2a172e22f5
Lucas Kusman Leal <lucaskleal222@outlook.com>
2022-03-27 16:14:01 -0300

    First-Calculator 0.1
    -Adicionado text label para o visor da calculadora, mostrando os valores inseridos, operação selecionada e o resultado final.
    -Adicionado função do botão Clear (C), responsável por reinicializar todas as variáveis necessárias para começar uma nova operação.
    -Alterado para o tipo Float todos os valores utilizados internamente para realizar as operações.

--- FirstCalculator/FirstCalculator/ViewController.swift
+++ FirstCalculator/FirstCalculator/ViewController.swift
@@ -14,54 +14,123 @@ class ViewController: UIViewController {
     var val2:Int?
     var operation:String?
     var result:Float?
-
+    var isResult:Bool?
+    var visorText:String?
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         actualValue = ""
+        visorText = ""
+        isResult = false
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func button0(_ sender: UIButton) {
+        if(isResult!) {
+            visorText! = ""
+            isResult = false
+        }
         actualValue! += "0"
+        visorText! += "0"
+        textLabel.text = visorText!
     }
     
     @IBAction func button1(_ sender: UIButton) {
+        if(isResult!) {
+            visorText! = ""
+            isResult = false
+        }
         actualValue! += "1"
+        visorText! += "1"
+        textLabel.text = visorText!
     }
     
     @IBAction func button2(_ sender: UIButton) {
+        if(isResult!) {
+            visorText! = ""
+            isResult = false
+        }
         actualValue! += "2"
+        visorText! += "2"
+        textLabel.text = visorText!
     }
     
     @IBAction func button3(_ sender: UIButton) {
+        if(isResult!) {
+            visorText! = ""
+            isResult = false
+        }
         actualValue! += "3"
+        visorText! += "3"
+        textLabel.text = visorText!
     }
     
     @IBAction func button4(_ sender: UIButton) {
+        if(isResult!) {
+            visorText! = ""
+            isResult = false
+        }
         actualValue! += "4"
+        visorText! += "4"
+        textLabel.text = visorText!
     }
     
     @IBAction func button5(_ sender: UIButton) {
+        if(isResult!) {
+            visorText! = ""
+            isResult = false
+        }
         actualValue! += "5"
+        visorText! += "5"
+        textLabel.text = visorText!
     }
     
     @IBAction func button6(_ sender: UIButton) {
+        if(isResult!) {
+            visorText! = ""
+            isResult = false
+        }
         actualValue! += "6"
+        visorText! += "6"
+        textLabel.text = visorText!
     }
     
     @IBAction func button7(_ sender: UIButton) {
+        if(isResult!) {
+            visorText! = ""
+            isResult = false
+        }
         actualValue! += "7"
+        visorText! += "7"
+        textLabel.text = visorText!
     }
     
     @IBAction func button8(_ sender: UIButton) {
+        if(isResult!) {
+            visorText! = ""
+            isResult = false
+        }
         actualValue! += "8"
+        visorText! += "8"
+        textLabel.text = visorText!
     }
     
     @IBAction func button9(_ sender: UIButton) {
+        if(isResult!) {
+            visorText! = ""
+            isResult = false
+        }
         actualValue! += "9"
+        visorText! += "9"
+        textLabel.text = visorText!
     }
     
     @IBAction func buttonResult(_ sender: UIButton) {
+        
+        if(!isResult!) {
+            isResult = true
+        }
         
         val2 = Int(actualValue!)
         actualValue = ""
@@ -83,52 +152,69 @@ class ViewController: UIViewController {
         let strVal1 = String(val1!)
         let strVal2 = String (val2!)
         
+        visorText! += " = \(strResult)"
+        textLabel.text = visorText!
+        
         print("Result \(strVal1) \(operation!) \(strVal2) = \(strResult)")
     }
     
-    
     @IBAction func buttonSum(_ sender: UIButton) {
         operation = "sum"
+        visorText! += " + "
+        textLabel.text = visorText!
         val1 = Int(actualValue!)
         actualValue = ""
     }
     
     @IBAction func buttonSubtract(_ sender: UIButton) {
         operation = "subtract"
+        visorText! += " - "
+        textLabel.text = visorText!
         val1 = Int(actualValue!)
         actualValue = ""
     }
     
     @IBAction func buttonMultiply(_ sender: UIButton) {
         operation = "multiply"
+        visorText! += " * "
+        textLabel.text = visorText!
         val1 = Int(actualValue!)
         actualValue = ""
     }
     
     @IBAction func buttonDivide(_ sender: UIButton) {
         operation = "divide"
+        visorText! += " / "
+        textLabel.text = visorText!
         val1 = Int(actualValue!)
         actualValue = ""
     }
     
+    @IBAction func buttonClear(_ sender: UIButton) {
+        visorText = ""
+        actualValue = ""
+        textLabel.text = visorText!
+    }
+    
     func sum() -> Float {
         print("sum")
-        return Float(val1! + val2!)
+        return Float(Float(val1!) + Float(val2!))
     }
     
     func subtract() -> Float {
         print("subtract")
-        return Float(val1! - val2!)
+        return Float(Float(val1!) - Float(val2!))
     }
     
     func multiply() -> Float {
         print("multiply")
-        return Float(val1! * val2!)
+        return Float(Float(val1!) * Float(val2!))
     }
     
     func divide() -> Float {
         print("divide")
-        return Float(val1! / val2!)
+        return Float(Float(val1!) / Float(val2!))
     }
+    
     
 }
